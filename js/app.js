5f0a049ab0c672a90e78bb3b007d39ba6c13ceb0
annazeller <annalena.zeller@gmail.com>
2019-01-07 22:48:24 +0100

    map in header

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).foundation();
 $(document).ready(function() {
 	$('#pagepiling').pagepiling({
 	    //sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
-			sectionsColor: ['#111628', '#111628', '#111628', '#111628', '#111628'],
+		sectionsColor: ['#111628', '#111628', '#111628', '#111628', '#111628'],
 	    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
     	menu: '#navigation',
     	navigation: true
